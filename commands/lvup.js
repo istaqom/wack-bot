b0fc83b64ee49d422838bf53d0f7af45513f8b62
istaqom <istaqom@gmail.com>
2021-10-02 18:02:50 +0800

    Adding lvlup function

--- commands/lvup.js
+++ commands/lvup.js
@@ -1,31 +1,37 @@
-const Discord = require('discord.js');
+const LvUp = require('../data/lvup.json');
+var Table = require('cli-table');
+
+function numFormat(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+}
 
 module.exports = {
 	name: 'lvup',
 	description: 'Show level up material',
 	execute(message) {
-        const cvEmbed = new Discord.MessageEmbed()
-                .setColor('0x0099ff')
-                .setTitle('CV Calculation')
-                .addFields(
-                    {
-                        name: 'Critical Rate',
-                        value: `${crate}`,
-                        inline: true,
-                    },
-                    {
-                        name: 'Critical Damage',
-                        value: `${cdmg}`,
-                        inline: true,
-                    },
-                    {
-                        name: 'CV',
-                        value: `${cv}`,
-                        inline: true,
-                    },
-                )
-                .setTimestamp()
-
-        // return message.channel.send({ embed: lvEmbed });
+        var table = new Table({
+            head: ['Level', 'EXP', 'Mora', "Hero's Wit"]
+          , colWidths: [10, 12, 12, 12]
+        });
+        var arrData = [0, 0, 0];
+        
+        for (var prop in LvUp) {
+            lvlData = []
+        
+            lvlData.push(`${numFormat(LvUp[prop]["level"])}`);
+            lvlData.push(`${numFormat(LvUp[prop]["exp"])}`);
+            lvlData.push(`${numFormat(LvUp[prop]["mora"])}`);
+            lvlData.push(`${numFormat(LvUp[prop]["hero wit"])}`);
+        
+            table.push(lvlData);
+        
+            arrData[0] += LvUp[prop]["exp"];
+            arrData[1] += LvUp[prop]["mora"];
+            arrData[2] += LvUp[prop]["hero wit"];
+        }
+        
+        table.push(['Total', `${numFormat(arrData[0])}`, `${numFormat(arrData[1])}`, `${numFormat(arrData[2])}`]);
+        
+        return message.channel.send('.\n' + table.toString());
     }
 };
