63ae4500efc20e5ea01bef3a9c800ef4809dcea4
istaqom <istaqom@gmail.com>
2021-10-03 16:57:52 +0800

    fixing bugs from calc

--- commands/calc.js
+++ commands/calc.js
@@ -3,19 +3,23 @@ const Discord = require('discord.js');
 
 module.exports = {
     name : 'calc',
+    args : true,
+    usage : '<something to calculate>',
     aliases : ['c'],
     descrition : 'To calculate',
     execute(message, args) {
         if (args.length) {
             try {
-                calculate = strMath(`${args.replace(/\s/g,'')}`)
+                noSpaceArgs = args.replace(/\s/g,'');
+                calculate = strMath(`${noSpaceArgs}`)
 
                 const calcEmbed = new Discord.MessageEmbed()
                     .setColor('0x0099ff')
-                    .setDescription(`${args} = ${calculate}`)
+                    .setDescription(`${noSpaceArgs} = ${calculate}`)
 
                 return message.channel.send(calcEmbed)
             } catch(err) {
+                console.log(err)
                 return message.channel.send(`Uh-oh something went wrong`).then(msg =>{
                     msg.delete({timeout : 10000})
                 });
