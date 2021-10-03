f75aba0845b219d4e6531fad6df548a8c03ee340
istaqom <istaqom@gmail.com>
2021-10-03 15:58:17 +0800

    fixing aliases bug

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ client.on('message', async message => {
 
 	if (!message.content.startsWith(process.env.PREFIX) || message.author.bot) return;
 
-	const args = message.content.slice(process.env.PREFIX.length).trim().split(' ');
+	const args = message.content.slice(process.env.PREFIX.length).trim().split(/ +/);
 	const commandName = args.shift().toLowerCase();
 	
 	const command = client.commands.get(commandName)
