88e29b6494480ca55cc11e9bee07a533fc030fa2
istaqom <istaqom@gmail.com>
2021-10-05 18:34:45 +0800

    Adding help function

--- commands/help.js
+++ commands/help.js
@@ -15,14 +15,14 @@ module.exports = {
             data.push(commands.map(command => command.name).join(', '));
             data.push(`\nYou can send \`${prefix}help [command name]\` to get info on a specific command!`);
 
-            return message.author.send(data, { split: true })
+            return message.channel.send(data, { split: true })
                 .then(() => {
                     if (message.channel.type === 'dm') return;
-                    message.reply('I\'ve sent you a DM with all my commands!');
+                    // message.reply('I\'ve sent you a DM with all my commands!');
                 })
                 .catch(error => {
-                    console.error(`Could not send help DM to ${message.author.tag}.\n`, error);
-                    message.reply('it seems like I can\'t DM you! Do you have DMs disabled?');
+                    // console.error(`Could not send help DM to ${message.author.tag}.\n`, error);
+                    // message.reply('it seems like I can\'t DM you! Do you have DMs disabled?');
                 });
 		}
 
