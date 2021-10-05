1cd4d5d12808bc688ae0966d499c03d95208f445
istaqom <istaqom@gmail.com>
2021-10-05 18:47:09 +0800

    Updating help function

--- commands/help.js
+++ commands/help.js
@@ -28,11 +28,11 @@ module.exports = {
             return message.reply('that\'s not a valid command!');
         }
 
-        data.push(`**Name:** ${command.name}`);
+        data.push(`.\n**Name:** ${command.name}`);
 
         if (command.aliases) data.push(`**Aliases:** ${command.aliases.join(', ')}`);
         if (command.description) data.push(`**Description:** ${command.description}`);
-        if (command.usage) data.push(`**Usage:** ${prefix}${command.name} ${command.usage}`);
+        if (command.usage) data.push(`**Usage:** \`${prefix}${command.name} ${command.usage}\``);
 
         message.channel.send(data, { split: true });
 	},
