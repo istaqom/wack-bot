e4b8bebd397654a331ab4cdefa7bbd368b04e9b9
istaqom <istaqom@gmail.com>
2021-10-11 16:07:06 +0800

    update for cv

--- commands/cv.js
+++ commands/cv.js
@@ -43,7 +43,7 @@ module.exports = {
                     },
                     {
                         name: 'CV',
-                        value: `${cv}`,
+                        value: `${cv.toFixed(2)}`,
                         inline: true,
                     },
                 )
