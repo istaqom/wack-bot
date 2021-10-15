4346208a00138d1151fd4c8fa71ca186a5c34bb5
istaqom <istaqom@gmail.com>
2021-10-15 20:24:39 +0800

    Updating daily function

--- commands/daily.js
+++ commands/daily.js
@@ -81,6 +81,19 @@ module.exports = {
         .setDescription(word)
         .setTimestamp();
 
+      if (todayData["image"] != "None") {
+        var regex = /^([^\\]*)\.(\w+)$/;
+        var matches = todayData["image"].match(regex);
+
+        if (matches) {
+            var filename = matches[1];
+            var extension = matches[2];
+        }
+
+        embed.attachFiles([todayData["image"]]);
+        embed.setImage(`attachment://${filename+extension}`);
+      }
+
       return message.channel.send(embed);
     }
   },
